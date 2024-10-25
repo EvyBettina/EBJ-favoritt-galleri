4e2cf56c2eac18093176ec86c6ecd2a5af275e3e
EvyBettina <evybettina@hotmail.com>
2024-10-25 12:44:07 +0200

    setup click function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello!');
+let allImageCards = document.querySelectorAll(".card");
+
+for (let imageCard of allImageCards) {
+    let favoritButton = imageCard.querySelector("button");
+
+    favoritButton.addEventListener("click", () => {
+    toggleFavorited(imageCard);
+});
+    
+    console.log(imageCard);
+}
+
+
+
+function toggleFavorited(card){
+    console.log(card);
+}
