fd4fdeced97d8d4316e81bc91d0145ec7e19d05e
EvyBettina <evybettina@hotmail.com>
2024-10-25 12:51:16 +0200

    add event handlers to all image cards

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// Hent ut alle "kortene" fra HTML
 let allImageCards = document.querySelectorAll(".card");
 
+// For hvert av kortene, legg til en favoritt funksjon
 for (let imageCard of allImageCards) {
     let favoritButton = imageCard.querySelector("button");
 
-    favoritButton.addEventListener("click", () => {
+// Så legger vi til en egen fuksjon, arrow funksjon "() => {};
+
+favoritButton.addEventListener("click", () => {
     toggleFavorited(imageCard);
 });
     
@@ -11,7 +15,7 @@ for (let imageCard of allImageCards) {
 }
 
 
-
-function toggleFavorited(card){
+// Favoritt funksjonen
+function toggleFavorited(card) {
     console.log(card);
 }
